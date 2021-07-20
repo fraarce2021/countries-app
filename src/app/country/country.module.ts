9c8a5e08398b2ed095ab1bb0dd4cb9d478bf08b2
Francisco <danielarcesoftware@gmail.com>
2021-07-19 22:32:17 -0700

    Begin structure for CountriesApp

---
+++ src/app/country/country.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CapitalComponent } from './pages/capital/capital.component';
+import { CountryComponent } from './pages/country/country.component';
+import { RegionComponent } from './pages/region/region.component';
+import { CountryDetailComponent } from './pages/country-detail/country-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    CapitalComponent,
+    CountryComponent,
+    RegionComponent,
+    CountryDetailComponent
+  ],
+  exports: [
+    CapitalComponent,
+    CountryComponent,
+    RegionComponent,
+    CountryDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountryModule { }
